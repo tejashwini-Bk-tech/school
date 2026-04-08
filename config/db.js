016f52a57003884534e376f8efccac513404fd8f
tejashwini-Bk-tech <tejashwinikoli2005@gmail.com>
2026-04-09 00:20:55 +0530

    commit

--- config/db.js
+++ config/db.js
@@ -1,15 +1,13 @@
 import mysql from "mysql2/promise";
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "school_db",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+const pool = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
+
 
 export async function verifyConnection() {
   const connection = await pool.getConnection();
