5a48778904f87418d0dd94ef0ba7c5eb608f5fa2
tejashwini-Bk-tech <tejashwinikoli2005@gmail.com>
2026-04-10 00:06:20 +0530

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { verifyConnection, closePool } from "./config/db.js";
 import app from "./app.js";
 
-const port = Number(process.env.PORT || 5000);
+const port = process.env.PORT || 5000
 
 async function startServer() {
   await verifyConnection();
